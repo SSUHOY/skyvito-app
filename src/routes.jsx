65f383b7453c5a203e028c64e353a3913696e226
Semyon <125258352+SSUHOY@users.noreply.github.com>
2023-10-17 21:24:23 +0300

    creates route to authpage

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Main from './components/pages/mainPage'
+import { AuthPage } from './components/pages/authPage'
 
 const AppRoutes = ({user}) => {
   return (
    <Routes>
     <Route path='/' element={<Main/>}/>
-    {/* <Route path='/login' element={}/> */}
+    <Route path='/login' element={<AuthPage/>}/>
    </Routes>
   )
 }
